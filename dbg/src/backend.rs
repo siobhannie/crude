f91e039564ee83371520c67e45110d08aa58ac6b
siobhannie <iam@vonnie.dev>
2025-01-30 20:00:00 -0800

    idk that atomics are the best idea for this but i'm sticking with it for now

--- dbg/src/backend.rs
+++ dbg/src/backend.rs
@@ -1,8 +1,10 @@
-use std::{fs::File, io::Read, path::PathBuf, sync::mpsc::{channel, Receiver, Sender}, thread};
+use std::{fs::File, io::Read, panic::{catch_unwind, AssertUnwindSafe}, path::PathBuf, sync::{atomic::{AtomicU32, Ordering}, mpsc::{channel, Receiver, Sender}, Arc}, thread};
 
 use crude::Gamecube;
 
-pub fn start_emu(ipl_path: impl ToString) -> (Sender<Command>, Receiver<Message>) {
+pub type SharedInstructionBuffer = Arc<[(AtomicU32, AtomicU32)]>;
+
+pub fn start_emu(ipl_path: impl ToString, instruction_buffer: SharedInstructionBuffer, processor_state: Arc<SharedProcessorState>) -> (Sender<Command>, Receiver<Message>) {
     let mut bios_data = Vec::new();
     File::open(ipl_path.to_string()).unwrap().read_to_end(&mut bios_data).unwrap();
     let (tx, rx) = channel();
@@ -11,28 +13,84 @@ pub fn start_emu(ipl_path: impl ToString) -> (Sender<Command>, Receiver<Message>
 	let mut gamecube = Gamecube::new();
 	gamecube.load_bios(bios_data);
 
+	processor_state.update(&mut gamecube);
+	update_instruction_buffer(&mut gamecube, &instruction_buffer);
+	
 	loop {
-	    match rx.recv().unwrap() {
-		Command::Run => {
-		    crude::run(&mut gamecube);
-		},
-		Command::Step => {
-		    let instr = gamecube.read_u32(gamecube.cpu.cia, true);
-		    tx_m.send(Message::Instruction(instr)).unwrap();
-		    crude::step(&mut gamecube);
+	    let result = catch_unwind(AssertUnwindSafe(|| {
+		match rx.recv().unwrap() {
+		    Command::Run => {
+			loop {
+			    for _ in 0..500000 {
+				crude::step(&mut gamecube);
+			    }
+
+			    if let Ok(Command::Stop) = rx.try_recv() {
+				break;
+			    }
+			    update_instruction_buffer(&mut gamecube, &instruction_buffer);
+			    processor_state.update(&mut gamecube);
+			}
+		    },
+		    Command::Step => {
+			update_instruction_buffer(&mut gamecube, &instruction_buffer);
+			processor_state.update(&mut gamecube);
+			crude::step(&mut gamecube);
+		    },
+		    Command::Stop => {},
 		}
-	    }
+	    }));
+	    update_instruction_buffer(&mut gamecube, &instruction_buffer);
+	    processor_state.update(&mut gamecube);
 	}
     });
 
     (tx, rx_m)
 }
 
+pub struct SharedProcessorState {
+    pub cia: AtomicU32,
+    pub nia: AtomicU32,
+    pub gprs: Vec<AtomicU32>
+}
+
+impl SharedProcessorState {
+    pub fn new() -> Self {
+	let mut gprs = Vec::with_capacity(32);
+	gprs.resize_with(32, || AtomicU32::new(0));
+	Self {
+	    cia: AtomicU32::new(0),
+	    nia: AtomicU32::new(0),
+	    gprs,
+	}
+    }
+
+    pub fn update(&self, gc: &mut Gamecube) {
+	self.cia.store(gc.cpu.cia, Ordering::Relaxed);
+	self.nia.store(gc.cpu.nia, Ordering::Relaxed);
+	for (i, n) in gc.cpu.gprs.iter().enumerate() {
+	    self.gprs[i].store(*n, Ordering::Relaxed);
+	}
+    }
+}
+
+fn update_instruction_buffer(gc: &mut Gamecube, buffer: &SharedInstructionBuffer) {
+    let mut start = gc.cpu.cia.wrapping_sub(5 * 4);
+
+    for (addr, instr) in buffer.iter() {
+	addr.store(start, Ordering::Relaxed);
+	let instruction = gc.read_u32(start, true);
+	instr.store(instruction, Ordering::Relaxed);
+	start = start.wrapping_add(4);
+    }
+}
+
 pub enum Command {
     Run,
     Step,
+    Stop,
 }
 
 pub enum Message {
-    Instruction(u32)
+
 }
--- dbg/src/main.rs
+++ dbg/src/main.rs
@@ -2,16 +2,21 @@ mod ext;
 mod backend;
 mod disassembler;
 
-use std::env;
+use std::{env, sync::{atomic::{AtomicU32, Ordering}, Arc}};
 
-use backend::{start_emu, Command, Message};
+use backend::{start_emu, Command, Message, SharedInstructionBuffer, SharedProcessorState};
 use disassembler::disassemble;
 use ext::System;
-use imgui::Condition;
+use imgui::{Condition, ImColor32, StyleColor};
 use log::LevelFilter;
+use ppc750cl::Ins;
 
 fn main() {
-    let (emu_commander, emu_messages) = start_emu(env::args().nth(1).unwrap());
+    let mut instruction_data = Vec::with_capacity(20);
+    instruction_data.resize_with(20, || (AtomicU32::new(0), AtomicU32::new(0)));
+    let instruction_buffer: SharedInstructionBuffer = Arc::from(instruction_data);
+    let processor_state = Arc::new(SharedProcessorState::new());
+    let (emu_commander, emu_messages) = start_emu(env::args().nth(1).unwrap(), instruction_buffer.clone(), processor_state.clone());
     simple_logger::SimpleLogger::new().with_level(LevelFilter::Debug).init().unwrap();
     let mut message = String::from("Waiting...");
     System::new("shmeeeep :3").unwrap().run((), move |_, ui, _| {
@@ -21,20 +26,43 @@ fn main() {
 		if ui.button("Step") {
 		    emu_commander.send(Command::Step).unwrap();
 		}
+		ui.same_line();
 		if ui.button("Run") {
 		    emu_commander.send(Command::Run).unwrap();
+		}
+		ui.same_line();
+		if ui.button("Stop") {
+		    emu_commander.send(Command::Stop).unwrap();
 		}
 	    });
 
 	ui.window("Instructions")
 	    .size([400.0, 600.0], Condition::Always)
 	    .build(|| {
-		if let Ok(Message::Instruction(instr)) = emu_messages.try_recv() {
-		    let disasmed = disassemble(instr);
-		    *(&mut message) = format!("Current Instruction: {disasmed}");
+		let mut offset = 0f32;
+		for (i, (addr, instr)) in instruction_buffer.iter().enumerate() {
+		    let addr = addr.load(Ordering::Relaxed);
+		    let instr = Ins::new(instr.load(Ordering::Relaxed), addr);
+		    let text = format!("{addr:#010X}: {}", instr.simplified());
+					 
+		    if i == 5 {
+			let style = ui.push_style_color(StyleColor::Text, [0.1, 1.0, 0.1, 1.0]);
+			ui.text_wrapped(text);
+			style.pop();
+		    } else {
+			ui.text_wrapped(text);	
+		    }
 		}
+	    });
 
-		ui.text_wrapped(&message);
+	ui.window("Processor State")
+	    .size([400.0, 700.0], Condition::Always)
+	    .build(|| {
+		ui.text_wrapped(format!("cia: {:#010X}", processor_state.cia.load(Ordering::Relaxed)));
+		ui.text_wrapped(format!("nia: {:#010X}", processor_state.nia.load(Ordering::Relaxed)));
+		for (i, gpr) in processor_state.gprs.iter().enumerate() {
+		    ui.text_wrapped(format!("r{}: {:#010X}", i, gpr.load(Ordering::Relaxed)));
+		}
 	    });
     }).unwrap();
 }
